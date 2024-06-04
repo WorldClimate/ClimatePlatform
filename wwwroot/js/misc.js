971441f7a4c30af81d1ebfd6d889e006db4adb5c
Chris Nelson <dj.ccelson@gmail.com>
2024-06-04 12:27:49 -0500

    Fix margin issue

--- wwwroot/js/misc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#nav").on('click','li',function(){
-		// remove classname 'active' from all li who already has classname 'active'
-		$("#nav li.current").removeClass("current"); 
-		// adding classname 'active' to current click li 
-		$(this).addClass("current"); 
-	});
-});
