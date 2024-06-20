96bdc93ab3e21d61d48fa3b817276fae390e4beb
Chris Nelson <dj.ccelson@gmail.com>
2024-06-20 18:20:39 -0500

    fixing description location

--- obj/Debug/net8.0/ClimatePlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClimatePlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ee80f27345610eb0f8b4427624b25063124611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29a028f5b585de8be16778e42efa8b7f534d3fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
