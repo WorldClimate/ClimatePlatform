776ad50287d4ba9bac0fe8b69d4d4a91cf74ad69
Chris Nelson <dj.ccelson@gmail.com>
2024-06-20 20:39:31 -0500

    Fixing page title

--- obj/Debug/net8.0/ClimatePlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClimatePlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29a028f5b585de8be16778e42efa8b7f534d3fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bdc93ab3e21d61d48fa3b817276fae390e4beb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClimatePlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
