c140e3efd6325f4f8a1a8433dd7264aa76e539f6
Chris Nelson <dj.ccelson@gmail.com>
2024-08-19 16:04:50 -0500

    Adding Locations page

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,6 +1,27 @@
 
 export default function LocationsPage() {
     return (
-      <div>Locations Page</div>
+      <section id="main">
+					<div className="container">
+						<div className="row gtr-200">
+							<div className="col-12">
+									<section className="box highlight">
+										<header>
+											<h2>Location coverage</h2>
+											<p>Select a city to see detailed climate information & analytics</p>
+											<ul className="divided">
+												<li><h3><a href="/location/Jakarta">Jakarta</a></h3></li>
+												<li><h3><a href="/location/Lagos">Lagos</a></h3></li>
+												<li><h3><a href="/location/Mumbai">Mumbai</a></h3></li>
+												<li><h3><a href="/location/Newyork">New York</a></h3></li>
+												<li><h3><a href="/location/Oxford">Oxford</a></h3></li>
+												<li><h3><a href="/location/Sanfrancisco">San Francisco</a></h3></li>
+											</ul>
+										</header>
+									</section>
+							</div>
+						</div>
+					</div>
+				</section>
     )
   }
