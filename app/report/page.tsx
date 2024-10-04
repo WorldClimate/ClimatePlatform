3da506c08fc6c924db678948a8318de326fab4dc
Chris Nelson <dj.ccelson@gmail.com>
2024-10-04 09:23:20 -0500

    Removing persistence layer

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -5,8 +5,6 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 import ReportOverview from '@/components/Reports/ReportOverview';
 import ReportDataEntry from '@/components/Reports/ReportDataEntry';
 import { Suspense } from 'react'
@@ -19,20 +17,11 @@ const queryClient = new QueryClient({
   },
 })
 
-
-
 export default function ReportPage(req: NextRequest) {
       const [reportState, setReportState] = useState({city_name: "", company_name: "", country_name: "", industry: "", mock_data: false, inputComplete: false});
-      if(typeof window !=='undefined'){
-       const persister = createSyncStoragePersister({
-          storage: window.localStorage,
-        })
       return (
         <Suspense fallback={<img src="/images/loading-spinner.svg" className="m-auto"/>}>
-          <PersistQueryClientProvider
-    client={queryClient}
-    persistOptions={{ persister }}
-  >
+          <QueryClientProvider client={queryClient}>
             <section id="main">
               <div className="px-10">
                 {reportState.inputComplete ? <ReportOverview city_name={reportState.city_name} company_name={reportState.company_name} 
@@ -40,8 +29,7 @@ export default function ReportPage(req: NextRequest) {
                                 : <ReportDataEntry setReportState={setReportState}/>}
               </div>
             </section>
-          </PersistQueryClientProvider>
+          </QueryClientProvider>
         </Suspense>
     );
-  }
 }
